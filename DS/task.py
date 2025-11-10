59c66329638f2dd535fa5f3e54b0ffcbee7ba199
Anand7879 <Anand7879@users.noreply.github.com>
2025-11-10 16:03:59 +0530

    list practice

--- DS/task.py
+++ DS/task.py
@@ -1,7 +1,7 @@
-lst = ['Mango','Apple','Papaya','banana','kiwi']
-lst1 = [10,40,20,50,30]
+# lst = ['Mango','Apple','Papaya','banana','kiwi']
+# lst1 = [10,40,20,50,30]
 
-l = len(lst)
+# l = len(lst)
 
 # print(lst[0])
 # print(lst[l-1])
@@ -92,7 +92,7 @@ l = len(lst)
 # print(lst1.count(10))
 
 
-lst = ['x','y','z','a','y','x']
+# lst = ['x','y','z','a','y','x']
 # find first index of y without function index
 # print(lst.index('y'))
 # for i in range(len(lst)):
@@ -120,13 +120,93 @@ lst = ['x','y','z','a','y','x']
 
 
 
-numbers = [10, 20, 30, 40, 50,60,70,80]
+# numbers = [10, 20, 30, 40, 50,60,70,80]
 
 #print all numbers
-print(numbers[:]) 
+# print(numbers[:]) 
 
 #print from index 0 to index 3
-print(numbers[2:5])
+# print(numbers[2:5])
+
+
+
+
+# Date:-10/11/2025
+
+lst = [2,3,2,4,3,5,5]
+# new_lst = []
+# for i in lst:
+#     if i not in new_lst:
+#         new_lst.append(i)   
+# print("original list:",lst)
+# print("list after removing duplicates:",new_lst)
+
+
+#two sum problem
+# target = 6
+# for i in range(len(lst)):
+#     for j in range(i+1,len(lst)):
+#         if lst[i] + lst[j] == target:
+#             print("index:",i,j)
+#             print("numbers:",lst[i],lst[j])
+#             break
+
+# optimized approach
+# seen = {}
+# for i in range(len(lst)):
+#     complement = target - lst[i]
+#     if complement in seen:
+#         print("index:",seen[complement],i)
+#         print("numbers:",complement,lst[i])
+#     seen[lst[i]] = i
+
+#running sum
+# a = [1,2,3,4]
+# lst = []
+# sum = 0
+
+# for i in a:
+#     sum+=i
+#     lst.append(sum)
+# print(lst)   
+# 
+
+
+# move all zeros to end without changing the order of other elements inplace
+# lst2 = [2,4,0,1,0,2,0,4,3,0]
+# new_lst = []
+# count = 0
+# for i in lst2:
+#     if i != 0:
+#         new_lst.append(i)
+#     else:
+#         count += 1
+# for i in range(count):
+#     new_lst.append(0) 
+# print("original list:",lst2)
+# print("list after moving zeros to end:",new_lst)
+
+
+
+
+# optimized approach inplace
+
+lst2 = [2,4,0,1,0,2,0,4,3,0]
+index = 0
+for i in range(len(lst2)):
+    if lst2[i] != 0:
+        lst2[index] = lst2[i]
+        index += 1
+for i in range(index, len(lst2)):
+    lst2[i] = 0
+print("list after moving zeros to end:",lst2)
+
+
+
+
+
+
+
 
 
 
---
+++ DS/tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+
+# lst = [2,3,2,4,3,5,5]
+# new_lst = []
+# for i in lst:
+#     if i not in new_lst:
+#         new_lst.append(i)   
+# print("original list:",lst)
+# print("list after removing duplicates:"
